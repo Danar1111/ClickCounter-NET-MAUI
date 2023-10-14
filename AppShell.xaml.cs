d9519613943b69e65f6106711208aa0947debfed
Danar <priyambodo02@gmail.com>
2023-10-14 19:19:12 +0700

    Submit

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MauiApps;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
---
+++ Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MauiApps;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
